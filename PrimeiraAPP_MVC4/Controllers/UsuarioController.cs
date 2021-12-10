4139133f6129996f86ffb4da7bb00a94e4d1b5e2
Rafael de Paula <rafael.paula@netviewinformatica.com.br>
2021-12-10 13:30:15 -0300

    Add project files.

---
+++ PrimeiraAPP_MVC4/Controllers/UsuarioController.cs
@@ -0,0 +1,30 @@
+﻿using System.Web.Mvc;
+using PrimeiraAPP_MVC4.Models;
+
+namespace PrimeiraAPP_MVC4.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        //
+        // GET: /Usuario/
+
+        private static Usuarios _usuarios = new Usuarios();
+
+        public ActionResult Index()
+        {
+            return View( _usuarios.listaUsuarios);
+        }
+
+        public ActionResult AdicionaUsuario()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AdicionaUsuario(UsuarioModel _usuarioModel)
+        {
+            _usuarios.CriaUsuario(_usuarioModel);
+            return View();
+        }
+    }
+}
---
+++ PrimeiraAPP_MVC4/Models/UsuarioModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace PrimeiraAPP_MVC4.Models
+{
+    public class UsuarioModel
+    {
+        [DisplayName("Primeiro Nome")]
+        [StringLength(50, ErrorMessage = "O campo Nome permite no máximo 50 caracteres!")]	        
+        public string nome { get; set; }
+        [Required]
+        public string sobrenome { get; set; }
+        public string endereco { get; set; }
+        [StringLength (50)]
+        [Required(ErrorMessage="Informe o Email")]
+        [RegularExpression(@"\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*", ErrorMessage = "Email inválido.")]
+        public string email { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime nascimento { get; set; }
+    }
+}
